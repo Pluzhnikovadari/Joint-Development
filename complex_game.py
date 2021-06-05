c803b56379a9f1ea81e1bee967742ec012dee658
Andrej <72415904+Tsalraavkaan@users.noreply.github.com>
2021-06-05 14:03:40 +0300

    Небольшие изменения
    1) исправлен баг со стенкой
    2) код переписан по flake8

--- complex_game.py
+++ complex_game.py
@@ -1,7 +1,7 @@
 from random import randint, random
 import pygame
 import sys
- 
+
 WIN_WIDTH = 1500
 WIN_HEIGHT = 1000
 WALL_WIDTH = 30
@@ -10,21 +10,20 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 FPS = 60
 
+
 def intercect_line_circle(x0, y0, r, x1, y1, x2, y2):
     '''проверяем есть ли решения у системы уравнений
     (y - y1) / (y2 - y1) = (x - x1) / (x2 - x1) и
     (x - x0)^2 + (y - y0)^2 = r^2'''
 
     if x1 == x2:
-        b = y0
+        b = -y0
         D = r**2 - (x1 - x0)**2
         if D < 0:
             return False
-        '''PROBLEM HERE!!!
         res1, res2 = -b - D**0.5, -b + D**0.5
         if res2 < y1 or res1 > y2:
-            print(y1, y2, res1, res2)
-            return False'''
+            return False
 
     else:
         a2 = (y1 - y2) / (x1 - x2)
@@ -42,8 +41,8 @@ def intercect_line_circle(x0, y0, r, x1, y1, x2, y2):
         if res2 < x1 or res1 > x2:
             return False
     return True
-        
- 
+
+
 class Wall:
     def __init__(self, surface, start, params, color):
         self.surf = surface
@@ -71,64 +70,73 @@ class Ball:
         pygame.draw.circle(self.surf, self.color, self.pos, self.radius)
 
     def move(self, objects):
-        flag = True
         for obj in objects:
             if obj.type == "ball":
-                if (self.pos[0] + self.dx - obj.pos[0]) ** 2 \
-                        + (self.pos[1] + self.dy - obj.pos[1]) ** 2 \
-                        <= (self.radius + obj.radius) ** 2 \
-                        and (self.pos[0] != obj.pos[0] \
-                        or self.pos[1] != obj.pos[1]):
+                x = self.pos[0] + self.dx - obj.pos[0]
+                y = self.pos[1] + self.dy - obj.pos[1]
+                r = self.radius + obj.radius
+                if x**2 + y**2 <= r**2 and \
+                   (self.pos[0] != obj.pos[0] or self.pos[1] != obj.pos[1]):
                     tmp = (self.dx, self.dy)
                     dif_rad = self.radius ** 2 - obj.radius ** 2
                     sum_rad = self.radius ** 2 + obj.radius ** 2
-                    self.dx = (dif_rad * self.dx + \
-                               2 * (obj.radius ** 2) * obj.dx) / sum_rad
-                    self.dy = (dif_rad * self.dy + \
-                               2 * (obj.radius ** 2) * obj.dy) / sum_rad
-                    obj.dx = (-dif_rad * obj.dx + \
-                              2 * (self.radius ** 2) * tmp[0]) / sum_rad
-                    obj.dy = (-dif_rad * obj.dy + \
-                              2 * (self.radius ** 2) * tmp[1]) / sum_rad
+                    self.dx = dif_rad * self.dx + 2 * (obj.radius ** 2) \
+                                                    * obj.dx
+                    self.dx /= sum_rad
+                    self.dy = dif_rad * self.dy + 2 * (obj.radius ** 2) \
+                                                    * obj.dy
+                    self.dy /= sum_rad
+                    obj.dx = -dif_rad * obj.dx + 2 * (self.radius ** 2) \
+                                                   * tmp[0]
+                    obj.dx /= sum_rad
+                    obj.dy = -dif_rad * obj.dy + 2 * (self.radius ** 2) \
+                                                   * tmp[1]
+                    obj.dy /= sum_rad
                     obj.pos = (obj.pos[0] + obj.dx, obj.pos[1] + obj.dy)
             if obj.type == "wall":
-                if intercect_line_circle(self.pos[0] + self.dx, self.pos[1] \
-                                         + self.dy, self.radius,
-                                         *obj.start, obj.start[0] \
-                                         + obj.params[0], obj.start[1]) or \
-                   intercect_line_circle(self.pos[0] + self.dx, self.pos[1] \
-                                         + self.dy, self.radius,
-                                         obj.start[0], obj.start[1] \
-                                         + obj.params[1], obj.start[0] \
-                                         + obj.params[0], obj.start[1] \
-                                         + obj.params[1]):
-                    self.dy *= -1
-                if intercect_line_circle(self.pos[0] + self.dx, self.pos[1] \
-                                         + self.dy, self.radius,
-                                         *obj.start, obj.start[0], \
-                                         obj.start[1] + obj.params[1]) or \
-                   intercect_line_circle(self.pos[0] + self.dx, self.pos[1] \
-                                         + self.dy, self.radius,
+                if intercect_line_circle(self.pos[0] + self.dx,
+                                         self.pos[1] + self.dy,
+                                         self.radius, *obj.start,
                                          obj.start[0] + obj.params[0],
-                                         obj.start[1], obj.start[0] \
-                                         + obj.params[0], obj.start[1] \
-                                         + obj.params[1]):
+                                         obj.start[1]) or \
+                   intercect_line_circle(self.pos[0] + self.dx,
+                                         self.pos[1] + self.dy,
+                                         self.radius, obj.start[0],
+                                         obj.start[1] + obj.params[1],
+                                         obj.start[0] + obj.params[0],
+                                         obj.start[1] + obj.params[1]):
+                    self.dy *= -1
+                if intercect_line_circle(self.pos[0] + self.dx,
+                                         self.pos[1] + self.dy,
+                                         self.radius, *obj.start, obj.start[0],
+                                         obj.start[1] + obj.params[1]) or \
+                   intercect_line_circle(self.pos[0] + self.dx,
+                                         self.pos[1] + self.dy,
+                                         self.radius,
+                                         obj.start[0] + obj.params[0],
+                                         obj.start[1],
+                                         obj.start[0] + obj.params[0],
+                                         obj.start[1] + obj.params[1]):
                     self.dx *= -1
 
         self.pos = (self.pos[0] + self.dx, self.pos[1] + self.dy)
         pygame.draw.circle(self.surf, self.color, self.pos, self.radius)
         return objects
 
+
 def init_walls(surface):
     wall_list = []
     wall_list.append(Wall(surface, (0, 0), (WALL_WIDTH, WIN_HEIGHT), RED))
     wall_list.append(Wall(surface, (0, 0), (WIN_WIDTH, WALL_WIDTH), RED))
-    wall_list.append(Wall(surface, (WIN_WIDTH - WALL_WIDTH, 0), (WALL_WIDTH, WIN_HEIGHT), RED))
-    wall_list.append(Wall(surface, (WALL_WIDTH, WIN_HEIGHT - WALL_WIDTH), (WIN_WIDTH, WALL_WIDTH), RED))
-    wall_list.append(Wall(surface, (WIN_WIDTH // 2, WIN_HEIGHT // 2), (WALL_WIDTH, WALL_WIDTH), RED))
+    wall_list.append(Wall(surface, (WIN_WIDTH - WALL_WIDTH, 0),
+                          (WALL_WIDTH, WIN_HEIGHT), RED))
+    wall_list.append(Wall(surface, (WALL_WIDTH, WIN_HEIGHT - WALL_WIDTH),
+                          (WIN_WIDTH, WALL_WIDTH), RED))
+    wall_list.append(Wall(surface, (WIN_WIDTH // 2, WIN_HEIGHT // 2),
+                          (WALL_WIDTH, WALL_WIDTH), RED))
     return wall_list
- 
- 
+
+
 sc = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 sc.fill(BLACK)
 pygame.display.update()
@@ -148,32 +156,30 @@ while 1:
             radius = random() * 10 + 20
             flag = True
             for obj in obj_list:
-                if obj.type == "ball":
-                    if (pos[0] - obj.pos[0]) ** 2 + (pos[1] - obj.pos[1])**2 \
-                        <= (radius + obj.radius) ** 2:
-                        flag = False
-                        break
+                if obj.type == "ball" and \
+                   (pos[0] - obj.pos[0]) ** 2 + \
+                   (pos[1] - obj.pos[1])**2 <= (radius + obj.radius) ** 2:
+                    flag = False
+                    break
                 if obj.type == "wall":
                     dif = (pos[0] - obj.start[0], pos[1] - obj.start[1])
                     if 0 <= dif[0] <= obj.params[0] and \
-                           0 <= dif[1] <= obj.params[1] or \
-                           intercect_line_circle(*pos, radius, *obj.start, \
-                                                 obj.start[0], obj.start[1] \
-                                                 + obj.params[1]) or \
-                           intercect_line_circle(*pos, radius, obj.start[0] \
-                                                 + obj.params[0], \
-                                                 obj.start[1], obj.start[0] \
-                                                 + obj.params[0], \
-                                                 obj.start[1] + \
-                                                 obj.params[1]) or \
-                           intercect_line_circle(*pos, radius,
-                                                 *obj.start, obj.start[0] \
-                                                 + obj.params[0], obj.start[1]) or \
-                           intercect_line_circle(*pos, radius,
-                                                 obj.start[0], obj.start[1] \
-                                                 + obj.params[1], obj.start[0] \
-                                                 + obj.params[0], obj.start[1] \
-                                                 + obj.params[1]):
+                       0 <= dif[1] <= obj.params[1] or \
+                       intercect_line_circle(*pos, radius, *obj.start,
+                                             obj.start[0],
+                                             obj.start[1] + obj.params[1]) or \
+                       intercect_line_circle(*pos, radius,
+                                             obj.start[0] + obj.params[0],
+                                             obj.start[1],
+                                             obj.start[0] + obj.params[0],
+                                             obj.start[1] + obj.params[1]) or \
+                       intercect_line_circle(*pos, radius, *obj.start,
+                                             obj.start[0] + obj.params[0],
+                                             obj.start[1]) or \
+                       intercect_line_circle(*pos, radius, obj.start[0],
+                                             obj.start[1] + obj.params[1],
+                                             obj.start[0] + obj.params[0],
+                                             obj.start[1] + obj.params[1]):
                         flag = False
                         break
             if flag:
@@ -191,5 +197,5 @@ while 1:
         if elem.type == "wall":
             elem.draw()
     obj_list = tmp
-        
+
     pygame.display.update()
