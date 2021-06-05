c37ebf075e1671ed937224239a71fe5ad3893aca
Andrej <72415904+Tsalraavkaan@users.noreply.github.com>
2021-06-05 05:46:16 +0300

    Улучшение физики взаимодействия
    1) Незначительный изменения в примере с шариком, финальная версия
    2) Переработка этого примера в более полноценную игру:
    2.1) добавлен класс стен, от которых может отскакивать мяч
    2.2) первая версия физики этих стен(осталось исправить баг для вертикальных стенок)

--- bubbles.py
+++ bubbles.py
@@ -6,18 +6,17 @@ WIN_WIDTH = 1500
 WIN_HEIGHT = 1000
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+FPS = 60
  
  
-class Circle:
+class Ball:
     def __init__(self, surface, pos, color, radius,
                  speed=None, dx=None, dy=None):
-        """Конструктору необходимо передать
-        поверхность, по которой будет летать
-        ракета и цвет самой ракеты"""
         self.surf = surface
         self.pos = pos
         self.color = color
         self.radius = radius
+        self.type = "ball"
         self.speed = 5 * random() + 2 if not speed else speed
         self.dx = 2 * self.speed * random() - self.speed if not dx else dx
         self.dy = 2 * self.speed * random() - self.speed if not dy else dy
@@ -29,32 +28,28 @@ class Circle:
             self.pos = (0, self.pos[1]) if self.pos[0] < 0 \
                                         else (WIN_WIDTH, self.pos[1])
             self.dx *= -1
-            flag = False
-        if self.pos[1] < 0 or self.pos[1] > WIN_HEIGHT:
+        elif self.pos[1] < 0 or self.pos[1] > WIN_HEIGHT:
             self.pos = (self.pos[0], 0) if self.pos[1] < 0 \
                                         else (self.pos[0], WIN_HEIGHT)
             self.dy *= -1
-            flag = False
-        for obj in objects:
-            if (self.pos[0] + self.dx - obj.pos[0]) ** 2 \
-                    + (self.pos[1] + self.dy - obj.pos[1]) ** 2 \
-                    <= (self.radius + obj.radius) ** 2 \
-                    and (self.pos[0] != obj.pos[0] or self.pos[1] != obj.pos[1]):
-                tmp = (self.dx, self.dy)
-                dif_rad = self.radius ** 2 - obj.radius ** 2
-                sum_rad = self.radius ** 2 + obj.radius ** 2
-                self.dx = (dif_rad * self.dx + \
-                           2 * (obj.radius ** 2) * obj.dx) / sum_rad
-                self.dy = (dif_rad * self.dy + \
-                           2 * (obj.radius ** 2) * obj.dy) / sum_rad
-                obj.dx = (-dif_rad * obj.dx + \
-                          2 * (self.radius ** 2) * tmp[0]) / sum_rad
-                obj.dy = (-dif_rad * obj.dy + \
-                          2 * (self.radius ** 2) * tmp[1]) / sum_rad
-                obj.pos = (obj.pos[0] + obj.dx, obj.pos[1] + obj.dy)
-                self.pos = (self.pos[0] + self.dx, self.pos[1] + self.dy)
-                flag = False
-        if flag:
+        else:
+            for obj in objects:
+                if (self.pos[0] + self.dx - obj.pos[0]) ** 2 \
+                        + (self.pos[1] + self.dy - obj.pos[1]) ** 2 \
+                        <= (self.radius + obj.radius) ** 2 \
+                        and (self.pos[0] != obj.pos[0] or self.pos[1] != obj.pos[1]):
+                    tmp = (self.dx, self.dy)
+                    dif_rad = self.radius ** 2 - obj.radius ** 2
+                    sum_rad = self.radius ** 2 + obj.radius ** 2
+                    self.dx = (dif_rad * self.dx + \
+                               2 * (obj.radius ** 2) * obj.dx) / sum_rad
+                    self.dy = (dif_rad * self.dy + \
+                               2 * (obj.radius ** 2) * obj.dy) / sum_rad
+                    obj.dx = (-dif_rad * obj.dx + \
+                              2 * (self.radius ** 2) * tmp[0]) / sum_rad
+                    obj.dy = (-dif_rad * obj.dy + \
+                              2 * (self.radius ** 2) * tmp[1]) / sum_rad
+                    obj.pos = (obj.pos[0] + obj.dx, obj.pos[1] + obj.dy)
             self.pos = (self.pos[0] + self.dx, self.pos[1] + self.dy)
         pygame.draw.circle(self.surf, self.color, self.pos, self.radius)
         return objects
@@ -65,8 +60,10 @@ sc.fill(BLACK)
 pygame.display.update()
 circle_list = []
 motion = True
+clock = pygame.time.Clock()
 
 while 1:
+    clock.tick(FPS)
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             sys.exit()
@@ -81,7 +78,7 @@ while 1:
                     flag = False
                     break
             if flag:
-                new_elem = Circle(sc, pos, color, radius)
+                new_elem = Ball(sc, pos, color, radius)
                 circle_list.append(new_elem)
         if i.type == pygame.KEYDOWN and i.key == pygame.K_SPACE:
             motion = False if motion else True
@@ -93,4 +90,3 @@ while 1:
             tmp = elem.move(tmp)
         circle_list = tmp
     pygame.display.update()
-    pygame.time.delay(20)
