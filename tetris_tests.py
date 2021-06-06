b44b36e2e8b22b33c811dad777be7b189cbee61d
Sasha-nice <71852540+Sasha-nice@users.noreply.github.com>
2021-06-06 15:45:06 +0300

    New game tetris
    Добавлена игра и тестовое покрытие

---
+++ tetris_tests.py
@@ -0,0 +1,32 @@
+import unittest
+from tetris import check_lost, create_grid, convert_shape_format, Piece
+from forms import T
+
+
+class TestGame(unittest.TestCase):
+
+    def test_check1(self):
+        pos = [(i, i + 1) for i in range(10)]
+        self.assertFalse(check_lost(pos))
+
+    def test_check2(self):
+        pos = [(i, i - 18) for i in range(10)]
+        self.assertTrue(check_lost(pos))
+
+    def test_create_grid(self):
+        loc = {}
+        grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if (j, i) in loc:
+                    grid[i][j] = 0
+        self.assertEqual(create_grid(loc), grid)
+
+    def test_convert_shape_format(self):
+        d = Piece(5, 0, T)
+        self.assertEqual(convert_shape_format(d), [(5, -3), (4, -2),
+                                                   (5, -2), (6, -2)])
+
+
+if __name__ == "__main__":
+    unittest.main()
